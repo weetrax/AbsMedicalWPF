7168c1c782d9dfbc77ab63dae18fc218155d6121
Stefano MARTINES <stefano.martines@hotmail.com>
2017-02-21 11:31:33 +0100

    Ajout des methodes Create et Update de la classe DoctorController

--- AbsMedical/src/AbsMedical/AbsMedical/Controllers/DoctorController.cs
+++ AbsMedical/src/AbsMedical/AbsMedical/Controllers/DoctorController.cs
@@ -10,6 +10,12 @@ namespace AbsMedical.Controllers
 {
     public class DoctorController
     {
+        /// <summary>
+        /// Get a doctor object in the Database by its email and password
+        /// </summary>
+        /// <param name="email">Email of the doctor</param>
+        /// <param name="password">Password of the doctor</param>
+        /// <returns>A doctor object</returns>
         public static doctor Find(string email, string password)
         {
             string md5Password = Encryption.GetMD5Hash(password);
@@ -18,6 +24,50 @@ namespace AbsMedical.Controllers
                 var query = db.doctor.FirstOrDefault(d => d.Email == email && d.Password == md5Password);
                 //var query2 = (from d in db.doctor where d.Email == email && d.Password == md5Password select d).FirstOrDefault();
                 return query;
+            }
+        }
+
+        /// <summary>
+        /// Add a doctor object in the Database
+        /// </summary>
+        /// <param name="doctor">doctor object to add</param>
+        /// <returns>Boolean indicating if the insertion was made</returns>
+        public static bool Create (doctor doctor)
+        {
+            using (rfidEntities db = new rfidEntities())
+            {
+                try
+                {
+                    db.doctor.Add(doctor);
+                    db.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Update doctor informations
+        /// </summary>
+        /// <param name="doctor">doctor object to update</param>
+        /// <returns>Boolean indicating if the insertion was made</returns>
+        public static bool Update (doctor doctor)
+        {
+            using (rfidEntities db = new rfidEntities())
+            {
+                try
+                {
+                    db.doctor.Attach(doctor);
+                    db.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
             }
         }
     }
