d5594730227c3e82023670b9fbca6e8b87022102
Stefano MARTINES <stefano.martines@hotmail.com>
2017-02-21 14:17:17 +0100

    ajout du projet

---
+++ src/AbsMedical/AbsMedical/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AbsMedical.Data;
+using AbsMedical.Controllers;
+using AbsMedical.Forms;
+using AbsMedical.Utils;
+
+namespace AbsMedical
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            string password = txtPassword.Text;
+            string email = txtEmail.Text;
+
+            doctor currentDoctor = DoctorController.Find(email, password);
+
+            if(currentDoctor != null)
+            {
+                HomeWindow form = new HomeWindow();
+                form.Show();
+                this.Close();
+            }
+            else
+            {
+                lblError.Visibility = Visibility.Visible;
+                lblError.Content = "Mail address or password invalid.";
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
