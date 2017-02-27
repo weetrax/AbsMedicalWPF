f4cde2423402a36f1fe197ec196cd343507229ab
Stefano MARTINES <stefano.martines@hotmail.com>
2017-02-27 10:56:15 +0100

    Ajout de la classe mail pour envoyer un mail

---
+++ src/AbsMedical/AbsMedical/Utils/Mail.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace AbsMedical.Utils
+{
+    public class Mail
+    {
+        private static string fromEmail = "martines.magnin@gmail.com";
+        private static string fromPassword = "stefanoadrien06";
+        //mail address: martines.magnin@gmail.com
+        //password: stefanoadrien06
+        public static bool Send(string to, string subject, StringBuilder body, Attachment attachment)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress(fromEmail);
+                mail.To.Add(to);
+                mail.Subject = subject;
+                mail.Body = body.ToString();
+
+                if (attachment != null)
+                {
+                    mail.Attachments.Add(attachment);
+                }
+
+                SmtpServer.Credentials = new NetworkCredential(fromEmail, fromPassword);
+                SmtpServer.Port = 25;
+                SmtpServer.EnableSsl = true;
+                SmtpServer.Send(mail);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        } 
+    }
+}
