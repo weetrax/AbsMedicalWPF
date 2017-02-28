64da3b120d22fbe1c77870f92780a0cc96fffba5
Stefano MARTINES <stefano.martines@hotmail.com>
2017-02-28 16:28:56 +0100

    quick update

--- src/AbsMedical/AbsMedical/Controllers/StudentController.cs
+++ src/AbsMedical/AbsMedical/Controllers/StudentController.cs
@@ -16,10 +16,10 @@ namespace AbsMedical.Controllers
         /// <returns>student object</returns>
         public static student Find(string studentGuid)
         {
-            rfidEntities db = new rfidEntities();
-            
-            return db.student.FirstOrDefault(w => w.Guid == studentGuid);
-            
+            using (rfidEntities db = new rfidEntities())
+            {
+                return db.student.FirstOrDefault(w => w.Guid == studentGuid);
+            }
         }
     }
 }
