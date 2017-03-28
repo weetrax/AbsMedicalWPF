fbfe10ab8ad2ba4637799ef74aca9d79a97f704e
Adrien MAGNIN <adrien.magnin@etu.unice.fr>
2017-03-28 16:00:24 +0200

    Modifications base de données

--- data/dataSQL.sql
+++ data/dataSQL.sql
@@ -9,19 +9,19 @@
 
 CREATE TABLE Student(
         Guid                 Varchar (100) NOT NULL ,
-        Lastname             Varchar (50) NOT NULL ,
+		StudentId            Varchar (50) UNIQUE NOT NULL ,
         Firstname            Varchar (50) NOT NULL ,
+        Lastname             Varchar (50) NOT NULL ,
         Email                Varchar (255) ,
         Phone                Varchar (10) NOT NULL ,
         Birthdate            Date NOT NULL ,
         Birthplace           Varchar (50) NOT NULL ,
-        StudentId            Varchar (50) NOT NULL ,
-        SocialSecurityNumber Varchar (50) NOT NULL ,
         Address              Varchar (100) NOT NULL ,
         PostalCode           Varchar (5) NOT NULL ,
         City                 Varchar (50) NOT NULL ,
-        CountryId                   Int NOT NULL ,
-        SchoolGuid          Varchar (100) NOT NULL ,
+        CountryId            Varchar (2) NOT NULL ,
+        SchoolGuid           Varchar (100) NOT NULL ,
+        SocialSecurityNumber Varchar (50) NOT NULL ,
         PRIMARY KEY (Guid )
 )ENGINE=InnoDB;
 
@@ -31,16 +31,17 @@ CREATE TABLE Student(
 #------------------------------------------------------------
 
 CREATE TABLE Doctor(
-        Guid       Varchar (100) NOT NULL ,
-        Lastname   Varchar (50) NOT NULL ,
-        Firstname  Varchar (50) NOT NULL ,
-        Address    Varchar (100) NOT NULL ,
-        PostalCode Varchar (7) NOT NULL ,
-        City       Varchar (50) NOT NULL ,
-        Email      Varchar (255) NOT NULL ,
-        Password   Varchar (255) NOT NULL ,
-        Phone      Varchar (10) NOT NULL ,
-        CountryId         Int NOT NULL ,
+        Guid                   Varchar (100) NOT NULL ,
+        Firstname              Varchar (50) NOT NULL ,
+        Lastname               Varchar (50) NOT NULL ,
+        Password               Varchar (255) NOT NULL ,
+        Email                  Varchar (255) NOT NULL ,
+        Phone                  Varchar (10) NOT NULL ,
+        Address                Varchar (100) NOT NULL ,
+        PostalCode             Varchar (7) NOT NULL ,
+        City                   Varchar (50) NOT NULL ,
+        CountryId              Varchar (2) NOT NULL ,
+        MailConfigurationGuid  Varchar (100),
         PRIMARY KEY (Guid )
 )ENGINE=InnoDB;
 
@@ -50,7 +51,7 @@ CREATE TABLE Doctor(
 #------------------------------------------------------------
 
 CREATE TABLE Country(
-        Id   Int NOT NULL ,
+        Id   Varchar (2) NOT NULL ,
         Name Varchar (50) NOT NULL ,
         PRIMARY KEY (Id )
 )ENGINE=InnoDB;
@@ -66,20 +67,10 @@ CREATE TABLE School(
         Address    Varchar (100) NOT NULL ,
         PostalCode Varchar (7) NOT NULL ,
         City       Varchar (50) NOT NULL ,
+        CountryId  Varchar (2) NOT NULL ,
         Email      Varchar (255) ,
         Phone      Varchar (25) ,
-        CountryId         Int NOT NULL ,
         PRIMARY KEY (Guid )
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Visit
-#------------------------------------------------------------
-
-CREATE TABLE Visit(
-        VisitDate Datetime NOT NULL ,
-        PRIMARY KEY (VisitDate )
 )ENGINE=InnoDB;
 
 
@@ -88,12 +79,11 @@ CREATE TABLE Visit(
 #------------------------------------------------------------
 
 CREATE TABLE MailConfiguration(
-        Guid        Varchar (100) NOT NULL ,
-        Email       Varchar (255) NOT NULL ,
-        Password    Varchar (255) NOT NULL ,
-        Smtp        Varchar (150) NOT NULL ,
-        Port        Int NOT NULL ,
-        DoctorGuid Varchar (100) NOT NULL ,
+        Guid     Varchar (100) NOT NULL ,
+        Email    Varchar (255) NOT NULL ,
+        Password Varchar (255) NOT NULL ,
+        Smtp     Varchar (150) NOT NULL ,
+        Port     Int NOT NULL ,
         PRIMARY KEY (Guid )
 )ENGINE=InnoDB;
 
@@ -103,20 +93,19 @@ CREATE TABLE MailConfiguration(
 #------------------------------------------------------------
 
 CREATE TABLE AbsMedical(
+		VisitDate    Datetime NOT NULL ,
+        StudentGuid  Varchar (100) NOT NULL ,
+        DoctorGuid   Varchar (100) NOT NULL ,
         StartDate    Datetime ,
         EndDate      Datetime ,
         Note         Varchar (255) NOT NULL ,
-        DoctorGuid         Varchar (100) NOT NULL ,
-        VisitDate    Datetime NOT NULL ,
-        StudentGuid Varchar (100) NOT NULL ,
-        PRIMARY KEY (DoctorGuid ,VisitDate ,StudentGuid )
+        PRIMARY KEY (VisitDate ,StudentGuid )
 )ENGINE=InnoDB;
 
 ALTER TABLE Student ADD CONSTRAINT FK_Student_Id FOREIGN KEY (CountryId) REFERENCES Country(Id);
 ALTER TABLE Student ADD CONSTRAINT FK_Student_Guid_School FOREIGN KEY (SchoolGuid) REFERENCES School(Guid);
 ALTER TABLE Doctor ADD CONSTRAINT FK_Doctor_Id FOREIGN KEY (CountryId) REFERENCES Country(Id);
+ALTER TABLE Doctor ADD CONSTRAINT FK_Doctor_Guid_MailConfiguration FOREIGN KEY (MailConfigurationGuid) REFERENCES MailConfiguration(Guid);
 ALTER TABLE School ADD CONSTRAINT FK_School_Id FOREIGN KEY (CountryId) REFERENCES Country(Id);
-ALTER TABLE MailConfiguration ADD CONSTRAINT FK_MailConfiguration_Guid_Doctor FOREIGN KEY (DoctorGuid) REFERENCES Doctor(Guid);
 ALTER TABLE AbsMedical ADD CONSTRAINT FK_AbsMedical_Guid FOREIGN KEY (DoctorGuid) REFERENCES Doctor(Guid);
-ALTER TABLE AbsMedical ADD CONSTRAINT FK_AbsMedical_VisitDate FOREIGN KEY (VisitDate) REFERENCES Visit(VisitDate);
 ALTER TABLE AbsMedical ADD CONSTRAINT FK_AbsMedical_Guid_Student FOREIGN KEY (StudentGuid) REFERENCES Student(Guid);
