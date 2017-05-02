8db4fa2a13ca948b03d7dd8fa6e92ffc0f1709e6
Stefano MARTINES <stefano.martines@hotmail.com>
2017-05-02 15:38:23 +0200

    Ajout de nouvelles onctionnalité + doc utilisateur

---
+++ src/AbsMedical/AbsMedical/Forms/ManageStudentsWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AbsMedical.Forms
+{
+    /// <summary>
+    /// Logique d'interaction pour ManageStudentsWindow.xaml
+    /// </summary>
+    public partial class ManageStudentsWindow : MetroWindow
+    {
+        public ManageStudentsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOurGitHub_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/weetrax/AbsMedicalWPF");
+        }
+        #region Dialog
+        private async void ShowAlert(string title)
+        {
+            var dialog = (BaseMetroDialog)this.Resources["CustomCloseDialog"];
+            dialog.Title = title;
+
+            await this.ShowMetroDialogAsync(dialog);
+            await dialog.WaitUntilUnloadedAsync();
+        }
+
+        private async void btnCloseCustomDialog_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = (BaseMetroDialog)this.Resources["CustomCloseDialog"];
+
+            await this.HideMetroDialogAsync(dialog);
+
+            RegisterStudentWindow window = new RegisterStudentWindow(lblLogedAs.Content.ToString());
+            window.Show();
+        }
+        #endregion
+    }
+}
